2a17dc3e1e34d61441d5a27ac11285b985182202
Samyra Rocha <samyra_srocha@hotmail.com>
2021-07-22 23:53:47 -0400

    Organizing Gradle and .gitignore

---
+++ app/src/main/java/com/example/projeto_integrador/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projeto_integrador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
