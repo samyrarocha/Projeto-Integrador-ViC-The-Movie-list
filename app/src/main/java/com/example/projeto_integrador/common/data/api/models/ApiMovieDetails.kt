c0f4ed9a9aeedf6bdb8d6dac95972bd6e3f10113
Samyra Rocha <samyra_srocha@hotmail.com>
2021-08-14 00:35:17 -0400

    Fixing details layout

--- app/src/main/java/com/example/projeto_integrador/common/data/api/models/ApiMovieDetails.kt
+++ app/src/main/java/com/example/projeto_integrador/common/data/api/models/ApiMovieDetails.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 data class ApiMovieDetails (
     @field:Json(name = "id") val detailsMovieId: Long?,
     @field:Json(name = "genres") val detailsGenreId: List<ApiGenre>?,
-    @field:Json(name = "poster_path") val detailsPosterPath: String?,
+    @field:Json(name = "backdrop_path") val detailsPosterPath: String?,
     @field:Json(name = "title") val detailsTitle:String?,
     @field:Json(name = "runtime") val detailsRuntime: Int?,
     @field:Json(name = "vote_average") val detailsVoteAverage: Float?,
