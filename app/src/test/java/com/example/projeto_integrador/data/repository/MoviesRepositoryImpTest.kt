09865f4fc82546127b0328e0842e57b287ec2c60
samyrarocha <samyra_srocha@hotmail.com>
2023-03-02 15:22:15 +0100

    Adding repository tests

---
+++ app/src/test/java/com/example/projeto_integrador/data/repository/MoviesRepositoryImpTest.kt
@@ -0,0 +1,98 @@
+package com.example.projeto_integrador.data.repository
+
+import com.example.projeto_integrador.data.api.models.ApiParameterValues
+import com.example.projeto_integrador.data.api.models.ApiParameters
+import com.example.projeto_integrador.data.api.models.TmdbApi
+import com.example.projeto_integrador.data.cache.Cache
+import com.example.projeto_integrador.data.mappers.ApiDiscoverMapper
+import com.example.projeto_integrador.data.mappers.ApiMovieDetailsMapper
+import com.example.projeto_integrador.data.mappers.ApiSearchMapper
+import com.example.projeto_integrador.data.stubs.*
+import com.example.projeto_integrador.presentation.feed.models.SearchParameters
+import kotlinx.coroutines.runBlocking
+import org.junit.*
+import org.junit.Assert.*
+import org.mockito.Mock
+import org.mockito.Mockito.*
+
+class MoviesRepositoryImpTest{
+
+
+    private lateinit var fakeMoviesRepositoryImp: MoviesRepositoryImp
+
+    @Mock
+    private val fakeApiDiscoverMapper = mock(ApiDiscoverMapper::class.java)
+    private val fakeApiSearchMapper = mock(ApiSearchMapper::class.java)
+    private val fakeApiMovieDetailsMapper = mock(ApiMovieDetailsMapper::class.java)
+    private val fakeApi = mock(TmdbApi::class.java)
+    private val fakeCache = mock(Cache::class.java)
+
+    @Before
+    fun setupRepository() {
+        fakeMoviesRepositoryImp = MoviesRepositoryImp(
+            fakeApi,
+            fakeApiDiscoverMapper,
+            fakeApiSearchMapper,
+            fakeApiMovieDetailsMapper,
+            fakeCache
+        )
+    }
+
+    @After
+    fun tearDown(){
+        clearAllCaches()
+    }
+
+    @Test
+    fun requestMovie_successful() = runBlocking {
+        //Given
+        val pageToLoad = 1
+        `when`(fakeApi.getDiscover(pageToLoad, null, ApiParameterValues.LANGUAGE_VALUE))
+            .thenReturn(getApiDiscoverStub())
+        `when`(fakeApiDiscoverMapper.mapToDomain(getApiDiscoverStub()))
+            .thenReturn(getDiscoverStub())
+
+        //When
+        val result = fakeMoviesRepositoryImp.requestMovies(pageToLoad, null)
+
+        //Then
+        assertEquals(result, getDiscoverStub())
+    }
+
+    @Test
+    fun searchMovies_successful() = runBlocking {
+        //Given
+        val pageToLoad = 1
+        val searchParameters = "wakanda"
+        val repositorySearchParameters = SearchParameters(query = "wakanda")
+        `when`(fakeApi.getSearch(pageToLoad, searchParameters, ""))
+            .thenReturn(getResultsForApiSearch())
+        `when`(fakeApiSearchMapper.mapToDomain(getResultsForApiSearch()))
+            .thenReturn(getResultsForSearch())
+
+        //When
+        val result = fakeMoviesRepositoryImp.searchMovies(pageToLoad, repositorySearchParameters)
+
+        //Then
+        assertEquals(result, getResultsForSearch())
+    }
+
+    @Test
+    fun getMovieDetails_successful() = runBlocking {
+        //Given
+        val movieId: Long = 550
+        `when`(fakeApi.getMovieDetails(
+            movieId = movieId,
+            dataLanguage = ApiParameterValues.LANGUAGE_VALUE,
+            appendToResponse = ApiParameters.CREDITS)
+        ).thenReturn(getApiMovieDetails())
+        `when`(fakeApiMovieDetailsMapper.mapToDomain(getApiMovieDetails()))
+            .thenReturn(getMovieDetails())
+
+        //When
+        val result = fakeMoviesRepositoryImp.getMovieDetails(movieId)
+
+        //Then
+        assertEquals(result, getMovieDetails())
+    }
+}
---
+++ app/src/test/java/com/example/projeto_integrador/data/stubs/Stubs.kt
@@ -0,0 +1,174 @@
+package com.example.projeto_integrador.data.stubs
+
+import com.example.projeto_integrador.data.api.models.*
+import com.example.projeto_integrador.domain.model.movies.*
+import com.example.projeto_integrador.domain.model.movies.details.MovieDetails
+import com.example.projeto_integrador.domain.model.movies.details.credits.Credits
+import com.example.projeto_integrador.domain.model.movies.details.credits.cast.Cast
+import com.example.projeto_integrador.domain.model.movies.details.credits.crew.Crew
+
+fun getApiDiscoverStub() = ApiDiscover(
+    discoverPage = 1,
+    discoverResults = getResultsForApiDiscover(),
+    numberOfItems = 2,
+    totalPages = 1
+)
+
+fun getResultsForApiDiscover() = listOf(
+    ApiMovie(
+        discoverPosterPath = "/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg",
+        discoverVoteAverage = 6.5F,
+        discoverMovieTitle = "Knock at the Cabin",
+        discoverMovieId = 631842
+    ),
+    ApiMovie(
+        discoverPosterPath = "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+        discoverVoteAverage = 7.4F,
+        discoverMovieTitle = "Black Panther: Wakanda Forever",
+        discoverMovieId = 505642
+    )
+)
+
+fun getDiscoverStub() = Discover(
+    page = 1,
+    movies = getResultsForDiscover(),
+    numberOfItems = 2,
+    totalPages = 1
+)
+
+fun getResultsForDiscover() = listOf(
+    Movie(
+        discoverMovieId = 631842,
+        discoverMovieTitle = "Knock at the Cabin",
+        discoverVoteAverage = 6.5F,
+        favorite = false,
+        discoverPosterPath = "/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg"
+    ),
+    Movie(
+        discoverPosterPath = "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+        discoverVoteAverage = 7.4F,
+        discoverMovieTitle = "Black Panther: Wakanda Forever",
+        discoverMovieId = 505642,
+        favorite = false
+    )
+)
+
+fun getResultsForApiSearch() = ApiSearch(
+    searchPage = 1,
+    searchResults = listOf(
+        ApiSearchResults(
+            searchPosterPath = "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+            searchVoteAverage = 7.4F,
+            searchMovieTitle = "Black Panther: Wakanda Forever",
+            searchMovieId = 505642,
+            searchGenresId = listOf(16, 10751, 12, 14)
+        )
+    ),
+    totalPages = 1,
+    totalResults = 1
+)
+
+fun getResultsForSearch() = Search(
+    searchPage = 1,
+    searchResults = listOf(
+        SearchResults(
+            searchPosterPath = "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+            searchVoteAverage = 7.4F,
+            searchMovieTitle = "Black Panther: Wakanda Forever",
+            searchMovieId = 505642,
+        )
+    ),
+    searchTotalPages = 1,
+    searchTotalResults = 1
+)
+
+fun getApiMovieDetails() = ApiMovieDetails(
+    detailsMovieId = 550,
+    detailsGenres = getGenreListForApiMovieDetails(),
+    detailsPosterPath = "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    detailsTitle = "Fight Club",
+    detailsRuntime = 139,
+    detailsVoteAverage = 8.431F,
+    overview = "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
+    releaseDate = "1999-10-15",
+    credits = getCreditsForApiMovieDetails()
+)
+
+fun getGenreListForApiMovieDetails() = listOf(
+    ApiGenre(
+        genreId = 18,
+        genreName = "Drama"
+    ),
+    ApiGenre(
+        genreId = 53,
+        genreName = "Thriller"
+    ),
+    ApiGenre(
+        genreId = 35,
+        genreName = "Comedy"
+    )
+)
+
+fun getCreditsForApiMovieDetails() = ApiCredits(
+    creditCast = listOf(
+        ApiCast(
+            castId = 819,
+            castCharacter = "The Narrator",
+            castPosterPath = "/5XBzD5WuTyVQZeS4VI25z2moMeY.jpg",
+            castName = "Edward Norton",
+        )
+    ),
+    creditCrew = listOf(
+        ApiCrew(
+            crewId = 7467,
+            crewPosterPath = "/tpEczFclQZeKAiCeKZZ0adRvtfz.jpg",
+            crewName = "David Fincher",
+            crewJob = "Director"
+        )
+    )
+)
+
+fun getMovieDetails() = MovieDetails(
+    detailsGenreName = getGenreListForMovieDetails(),
+    detailsBackDropPath = "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    detailsNameTitle = "Fight Club",
+    detailsRuntime = 139,
+    detailsVoteAverage = 8.431F,
+    detailsOverview = "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
+    detailsReleasedYear = "1999-10-15",
+    credits = getCreditsForMovieDetails()
+)
+
+fun getGenreListForMovieDetails() = listOf(
+    Genre(
+        genreId = 18,
+        genreName = "Drama"
+    ),
+    Genre(
+        genreId = 53,
+        genreName = "Thriller"
+    ),
+    Genre(
+        genreId = 35,
+        genreName = "Comedy"
+    )
+)
+
+fun getCreditsForMovieDetails() = Credits(
+    creditCast = listOf(
+        Cast(
+            castId = 819,
+            castCharacter = "The Narrator",
+            castPosterPath = "/5XBzD5WuTyVQZeS4VI25z2moMeY.jpg",
+            castName = "Edward Norton",
+        )
+    ),
+    creditCrew = listOf(
+        Crew(
+            crewId = 7467,
+            crewPosterPath = "/tpEczFclQZeKAiCeKZZ0adRvtfz.jpg",
+            crewName = "David Fincher",
+            crewJob = "Director"
+        )
+    )
+)
